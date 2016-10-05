117a6776f4fda91c66fe72fa5398334046f521d5
JGastl <jgastl@monusb.ca>
2016-10-05 09:23:55 -0500

    test

--- github_p5_test/sketch.js
+++ github_p5_test/sketch.js
@@ -8,8 +8,5 @@ function draw() {
 }
 function touchStarted() {
   background(255);
-  textSize(50);
-  text(touches[0].x, 50, 50);
-  text(touches[0].y, 50, 100);
-  rect(windowWidth/2, windowHeight/2, 40,40);
+  rect(touches[0].x, touches[0].y, 40,40);
 }
