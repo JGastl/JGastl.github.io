7bde1d0a4e8186f6e6a9cf5989125d7578786408
JGastl <jgastl@monusb.ca>
2016-10-05 10:28:49 -0500

    test

--- github_p5_test/sketch.js
+++ github_p5_test/sketch.js
@@ -9,7 +9,7 @@ function setup() {
 
 function draw() {
    background(255);
-    fill(random(0,255),random(0,255),random(0,255));
+    fill(255,0,0);
   for(var i=0;i<touches.length;i++){
    rect(touches[i].x, touches[i].y,200,200);
    line(touches[i].x, touches[i].y);
