647994f221dbfbf07e5836af3c672674f05d9181
JGastl <jgastl@monusb.ca>
2016-10-05 10:53:15 -0500

    test

--- github_p5_test/sketch.js
+++ github_p5_test/sketch.js
@@ -12,6 +12,11 @@ function draw() {
     fill(255,0,0);
   for(var i=0;i<touches.length;i++){
    rect(touches[i].x, touches[i].y,200,200);
+   if((i+1)<touches.length){
    line(touches[i].x, touches[i].y, touches[i+1].x, touches[i+1].y);
-}
+   } 
+   else{
+    line(touches[i].x, touches[i].y, touches[0].x, touches[0].y)
+   }
+  }
 }
