f452aed6b3bf834b11507de67805744ff7a9ee13
JGastl <jgastl@monusb.ca>
2016-10-05 09:30:19 -0500

    test

--- github_p5_test/sketch.js
+++ github_p5_test/sketch.js
@@ -1,7 +1,7 @@
 function setup() {
   createCanvas(windowWidth,windowHeight);
   rectMode(CENTER);
-  fill(random(0,255),random(0,255),random(0,255))
+
 }
 
 function draw() {
@@ -9,5 +9,6 @@ function draw() {
 }
 function touchStarted() {
   background(255);
+  fill(random(0,255),random(0,255),random(0,255));
   rect(touches[0].x, touches[0].y,400,400);
 }
