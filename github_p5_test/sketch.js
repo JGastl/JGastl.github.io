2aaa6ef3aa3be5e66cc23d3ef03cd0f054f573ba
JGastl <jgastl@monusb.ca>
2016-10-19 09:00:07 -0500

    test

--- github_p5_test/sketch.js
+++ github_p5_test/sketch.js
@@ -23,8 +23,6 @@ function draw() {
    beginShape();
    for(i=0;i<touches.length;i++){
     vertex(touches[i].x,touches[i].y);
-    vertex(touches[i+1].x,touches[i+1].y);
-    vertex(touches[0].x,touches[0].y);
    }
    endShape(CLOSE);
   }
