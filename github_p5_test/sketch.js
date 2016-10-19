9afe421e58d9578e68d747d551fe440ac1a64d75
JGastl <jgastl@monusb.ca>
2016-10-19 09:02:48 -0500

    test

--- github_p5_test/sketch.js
+++ github_p5_test/sketch.js
@@ -9,19 +9,19 @@ function setup() {
 
 function draw() {
    background(255);
-    noFill(0);
+    /*noFill(0);
   for(var i=0;i<touches.length;i++){
    rect(touches[i].x, touches[i].y,200,200);
-   /*if((i+1)<touches.length){
+   if((i+1)<touches.length){
    line(touches[i].x, touches[i].y, touches[i+1].x, touches[i+1].y);
    } 
    else{
     line(touches[i].x, touches[i].y, touches[0].x, touches[0].y)
-   }*/
-  }
+   }
+  }*/
    fill(255,0,0);
    beginShape();
-   for(i=0;i<touches.length;i++){
+   for(var i=0;i<touches.length;i++){
     vertex(touches[i].x,touches[i].y);
     vertex(touches[i+1].x,touches[i+1].y);
    }
