201b669cd6b1ff1f21dde655d8c016dff24270e7
JGastl <jgastl@monusb.ca>
2016-10-19 10:57:18 -0500

    test

--- rot_test/sketch.js
+++ rot_test/sketch.js
@@ -4,6 +4,7 @@ function setup() {
 
 function draw() {
  background(255);
+ ellipse(windowWidth/2,windowHeight/2, 100,100);
  textSize(40);
  text("Rx: " + floor(rotationX), 100, 100);
  text("Ry: " +floor(rotationY), 100, 150);
