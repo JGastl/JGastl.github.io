8639344d4ddead42b999c1953c7fc0d1b911264e
JGastl <jgastl@monusb.ca>
2016-10-26 14:09:07 -0500

    a

--- rot_test/sketch.js
+++ rot_test/sketch.js
@@ -2,7 +2,7 @@ var bSize,vitesseX,vitesseY,accelX,accelY,posX,posY,angX,angY,frict;
 function setup() {
    createCanvas(windowWidth,windowHeight);
    ellipseMode(CENTER);
-   bSize=200;
+   bSize=100;
    posX=windowWidth/2;
    posY=windowHeight/2;
    
@@ -18,7 +18,7 @@ function draw() {
  text("Ry: " +floor(rotationY), 100, 150);
  text("Rz: " + floor(rotationZ), 100, 200);
  
-  frict=0.00009;
+  frict=0.00015;
   accelX=rotationY*frict;
   accelY=rotationX*frict;
   vitesseX+=accelX;
