6de2d33deae2cb0eb321b50bb61737941844dcdc
JGastl <jgastl@monusb.ca>
2016-11-02 14:32:24 -0500

    w

--- rot_test/sketch.js
+++ rot_test/sketch.js
@@ -50,7 +50,12 @@ function draw() {
    vitesseX=-vitesseX//*bounce;
    //vitesseY=-vitesseY*bounce;
   }
- 
+    if(dist(posX,posY,obX2,obY2)<=bSize/2+obSize2/2){
+     score++5
+  }
+    if(dist(posX,posY,obX3,obY3)<=bSize/2+obSize3/2){
+   score--5
+  }
 }
 function drawBall(){
  fill(0,255,200);
@@ -64,10 +69,4 @@ function obs(){
  ellipse(obX2,obY2,obSize2,obSize2);
  fill(255,0,0);
  ellipse(obX3,obY3,obSize3,obSize3);
-     if(dist(posX,posY,obX2,obY2)<=bSize/2+obSize2/2){
-     score+=5
-  }
-    if(dist(posX,posY,obX3,obY3)<=bSize/2+obSize3/2){
-   score-=5
-  }
 }
