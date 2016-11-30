4f050961a066f4669c471a888d6b2af343ca3737
JGastl <jgastl@monusb.ca>
2016-11-30 10:54:09 -0600

    h

--- rot_test/sketch.js
+++ rot_test/sketch.js
@@ -1,39 +1,41 @@
-var bSize, vitesseX, vitesseY, accelX, accelY, posX, posY, angX, angY, frict, bounce, score,obstacles = [],img, type=0, typeobs, obsSize,obsX,obsY;
+var bSize, vitesseX, vitesseY, accelX, accelY, posX, posY, angX, angY, frict, bounce, score, obstacles = [],
+ img, type = 0,
+ typeobs, obsSize, obsX, obsY,distmin=20;
 
-function preload(){
- img=loadImage("back.jpg");
+function preload() {
+ img = loadImage("back.jpg");
 }
 
 function setup() {
  createCanvas(windowWidth, windowHeight);
  ellipseMode(CENTER);
  for (var i = 0; i < 3; i++) {
-  if(type===0){
-   typeobs="obs";
+  if (type === 0) {
+   typeobs = "obs";
    type++;
-  }
-  else if(type===1){
-   typeobs="bonus";
+  } else if (type === 1) {
+   typeobs = "bonus";
    type++;
+  } else {
+   typeobs = "malus";
+   type = 0;
   }
-  else{
-   typeobs="malus";
-   type=0;
-  }
-  obsSize=random(30,60);
-  var cycle=true;
-  while(cycle){
-  obsX=random(10,windowWidth);
-  obsY=random(10, windowHeight);
-  cycle=false
-  for(var j=0;j<i;j++){
-   if(dist(obsX,obsY,obstacles[j].obsX,obstacles[j].obsY<=obsSize/2+obstacles[j].size/2)){
-    cycle=true;
-    break;
+  
+  
+  obsSize = random(30, 60);
+  var cycle = true;
+  while (cycle) {
+   obsX = random(10, windowWidth);
+   obsY = random(10, windowHeight);
+   cycle = false
+   for (var j = 0; j < i; j++) {
+    if (dist(obsX, obsY, obstacles[j].obsX, obstacles[j].obsY <= obsSize / 2 + obstacles[j].obsSize / 2+distmin)) {
+     cycle = true;
+     break;
+    }
    }
   }
-  }
-  obstacles[i] = new Obs(typeobs,obsSize,obsX,obsY);
+  obstacles[i] = new Obs(typeobs, obsSize, obsX, obsY);
  }
  bSize = 50;
  score = 0;
@@ -45,11 +47,11 @@ function setup() {
 }
 
 function draw() {
- background(img,100);
+ background(img, 100);
  drawBall();
  fill(255);
  textSize(40);
- text("score:" + score, windowWidth/2, windowHeight/2);
+ text("score:" + score, windowWidth / 2, windowHeight / 2);
  for (i = 0; i < 3; i++) {
   obstacles[i].drawObs();
  }
@@ -78,45 +80,45 @@ function draw() {
   posY = bSize / 2;
  }
  for (i = 0; i < 3; i++) {
-    if (dist(posX, posY, obstacles[i].x,obstacles[i].y) <= bSize / 2 + obstacles[i].size / 2){
-    if(obstacles[i].type==="obs"){
+  if (dist(posX, posY, obstacles[i].x, obstacles[i].y) <= bSize / 2 + obstacles[i].size / 2) {
+   if (obstacles[i].type === "obs") {
     vitesseX = -vitesseX * bounce;
-     vitesseY = -vitesseY * bounce;
-    }else if (obstacles[i].type==="bonus") {
-      if (obstacles[i].col === false) {
-       obstacles[i].col = true
-       score += 5;
-      }
-     }else if (obstacles[i].type==="malus") {
-      if (obstacles[i].col === false) {
-       obstacles[i].col = true
-       score -= 5;
-      }
-     }
-   }
-    else {
-   obstacles[i].col = false;
+    vitesseY = -vitesseY * bounce;
+   } else if (obstacles[i].type === "bonus") {
+    if (obstacles[i].col === false) {
+     obstacles[i].col = true
+     score += 5;
     }
+   } else if (obstacles[i].type === "malus") {
+    if (obstacles[i].col === false) {
+     obstacles[i].col = true
+     score -= 5;
+    }
+   }
+  } else {
+   obstacles[i].col = false;
+  }
  }
 }
+
 function drawBall() {
  fill(0, 255, 255);
  ellipse(posX, posY, bSize, bSize);
 }
 
-function Obs(obstacle) {
+function Obs(obstacle, size, x, y) {
  this.type = obstacle;
- this.x = random(10, windowWidth);
- this.y = random(10, windowHeight);
- this.size = random(30, 60);
- this.col=false
-if(this.type==="obs"){
-   this.colour=color(0,0,255);
-    }else if (this.type==="bonus") {
-      this.colour=color(0,255,0);
-     }else{
-      this.colour=color(255,0,0);
-      }
+ this.x = x;
+ this.y = y;
+ this.size = size;
+ this.col = false
+ if (this.type === "obs") {
+  this.colour = color(0, 0, 255);
+ } else if (this.type === "bonus") {
+  this.colour = color(0, 255, 0);
+ } else {
+  this.colour = color(255, 0, 0);
+ }
  this.drawObs = function() {
   fill(this.colour)
   ellipse(this.x, this.y, this.size, this.size);
