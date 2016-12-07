f29a81c22ebd8b4a659e742a4b9665a47cccb7a1
Joshua Gastl <jgastl@monusb.ca>
2016-12-07 09:30:15 -0600

    j

--- rot_test/sketch.js
+++ rot_test/sketch.js
@@ -1,4 +1,4 @@
-var bSize, vitesseX, vitesseY, accelX, accelY, posX, posY, angX, angY, frict, bounce, score,obstacles = [],img, type=0, typeobs, obsSize,obsX,obsY, timer;
+var newGame,bSize, vitesseX, vitesseY, accelX, accelY, posX, posY, angX, angY, frict, bounce, score,obstacles = [],img, type=0, typeobs, obsSize,obsX,obsY, timer,level,iMax;
 
 function preload(){
  img=loadImage("back.jpg");
@@ -7,35 +7,8 @@ function preload(){
 function setup() {
  createCanvas(windowWidth, windowHeight);
  ellipseMode(CENTER);
+ level=0
  timer=16*60;
- for (var i = 0; i < 3; i++) {
-  if(type===0){
-   typeobs="obs";
-   type++;
-  }
-  else if(type===1){
-   typeobs="bonus";
-   type++;
-  }
-  else{
-   typeobs="malus";
-   type=0;
-  }
-  obsSize=random(30,60);
-  var cycle=true;
-  while(cycle){
-  obsX=random(10,windowWidth);
-  obsY=random(10, windowHeight);
-  cycle=false
-  for(var j=0;j<i;j++){
-   if(dist(obsX,obsY,obstacles[j].obsX,obstacles[j].obsY<=obsSize/2+obstacles[j].size/2)){
-    cycle=true;
-    break;
-   }
-  }
-  }
-  obstacles[i] = new Obs(typeobs,obsSize,obsX,obsY);
- }
  bSize = 50;
  score = 0;
  posX = windowWidth / 2;
@@ -44,19 +17,15 @@ function setup() {
  vitesseX = 0;
  vitesseY = 0;
 }
-
 function draw() {
  background(img,100);
+ newGame=false;
  timer-=1;
  drawBall();
  fill(255);
  textSize(25);
  text("score:" + score, 20, 30);
  text("Time:" + floor(timer/60), windowWidth/2, 30);
- 
- for (i = 0; i < 3; i++) {
-  obstacles[i].drawObs();
- }
  frict = 0.01;
  accelX = floor(rotationY) * frict;
  accelY = floor(rotationX) * frict;
@@ -64,7 +33,14 @@ function draw() {
  vitesseY += accelY;
  posX += vitesseX;
  posY += vitesseY;
-
+ if(level===0){
+   level=1;
+   newGame=true;
+ }
+if(newGame===true){
+  obstac();
+  newGame=false;
+}
  if (posX + bSize / 2 >= windowWidth) {
   vitesseX = -vitesseX * bounce;
   posX = windowWidth - bSize / 2;
@@ -81,7 +57,53 @@ function draw() {
   vitesseY = -vitesseY * bounce;
   posY = bSize / 2;
  }
- for (i = 0; i < 3; i++) {
+}
+function drawBall() {
+ fill(0, 255, 255);
+ ellipse(posX, posY, bSize, bSize);
+}
+function obstac(){
+  obsSize=random(30,60);
+  var cycle=true;
+if(level==1){
+  iMax=3
+}
+else if(level==2){
+  iMax=4
+}
+else if(level==3){
+  iMax=5
+}
+for (var i = 0; i < iMax; i++) {
+  if(type===0){
+   typeobs="obs";
+   type++;
+  }
+  else if(type===1){
+   typeobs="bonus";
+   type++;
+  }
+  else{
+   typeobs="malus";
+   type=0;
+  }
+}
+ for (i = 0; i < iMax; i++) {
+  obstacles[i].drawObs();
+ }
+    while(cycle){
+  obsX=random(10,windowWidth);
+  obsY=random(10, windowHeight);
+  cycle=false
+  for(var j=0;j<i;j++){
+   if(dist(obsX,obsY,obstacles[j].obsX,obstacles[j].obsY<=obsSize/2+obstacles[j].size/2)){
+    cycle=true;
+    break;
+   }
+  }
+  obstacles[i] = new Obs(typeobs,obsSize,obsX,obsY);
+ }
+ for (i = 0; i < iMax; i++) {
     if (dist(posX, posY, obstacles[i].x,obstacles[i].y) <= bSize / 2 + obstacles[i].size / 2){
     if(obstacles[i].type==="obs"){
     vitesseX = -vitesseX * bounce;
@@ -103,11 +125,6 @@ function draw() {
     }
  }
 }
-function drawBall() {
- fill(0, 255, 255);
- ellipse(posX, posY, bSize, bSize);
-}
-
 function Obs(obstacle) {
  this.type = obstacle;
  this.x = random(10, windowWidth);
