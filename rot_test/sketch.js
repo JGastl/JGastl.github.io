101f4857917fa4f60ee6ce721d748d81c7347e0b
Joshua Gastl <jgastl@monusb.ca>
2016-12-16 11:21:52 -0600

    m

--- rot_test/sketch.js
+++ rot_test/sketch.js
@@ -1,55 +1,15 @@
-<<<<<<< HEAD
-var bSize, vitesseX, vitesseY, accelX, accelY, posX, posY, angX, angY, frict, bounce, score, obstacles = [],
- img, type = 0,
- typeobs, obsSize, obsX, obsY,distmin=20;
-
-function preload() {
- img = loadImage("back.jpg");
-=======
 var i, newGame,bSize, vitesseX, vitesseY, accelX, accelY, posX, posY, angX, angY, frict, bounce, score,obstacles = [],img, type=0, typeobs, obsSize,obsX,obsY, timer,level,iMax;
 //////////////////////////////////////////////
 function preload(){
  img=loadImage("back.jpg");
->>>>>>> origin/master
 }
 ///////////////////////////////////////////////////////////////
 function setup() {
  createCanvas(windowWidth, windowHeight);
  ellipseMode(CENTER);
-<<<<<<< HEAD
- for (var i = 0; i < 3; i++) {
-  if (type === 0) {
-   typeobs = "obs";
-   type++;
-  } else if (type === 1) {
-   typeobs = "bonus";
-   type++;
-  } else {
-   typeobs = "malus";
-   type = 0;
-  }
-  
-  
-  obsSize = random(30, 60);
-  var cycle = true;
-  while (cycle) {
-   obsX = random(10, windowWidth);
-   obsY = random(10, windowHeight);
-   cycle = false
-   for (var j = 0; j < i; j++) {
-    if (dist(obsX, obsY, obstacles[j].obsX, obstacles[j].obsY <= obsSize / 2 + obstacles[j].obsSize / 2+distmin)) {
-     cycle = true;
-     break;
-    }
-   }
-  }
-  obstacles[i] = new Obs(typeobs, obsSize, obsX, obsY);
- }
-=======
  rectMode(CENTER)
  level=0
  timer=16*60;
->>>>>>> origin/master
  bSize = 50;
  score = 0;
  posX = windowWidth / 2;
@@ -60,15 +20,6 @@ function setup() {
 }
 //////////////////////////////////////////
 function draw() {
-<<<<<<< HEAD
- background(img, 100);
- drawBall();
- fill(255);
- textSize(40);
- text("score:" + score, windowWidth / 2, windowHeight / 2);
- for (i = 0; i < 3; i++) {
-  obstacles[i].drawObs();
-=======
    if(level===0){
    level=1;
    newGame=true;
@@ -83,7 +34,6 @@ if(newGame===true){
  drawBall();
  for(i = 0; i < iMax; i++){
  obstacles[i].drawObs();
->>>>>>> origin/master
  }
  fill(255);
  textSize(25);
@@ -114,11 +64,6 @@ if(newGame===true){
   vitesseY = -vitesseY * bounce;
   posY = bSize / 2;
  }
-<<<<<<< HEAD
- for (i = 0; i < 3; i++) {
-  if (dist(posX, posY, obstacles[i].x, obstacles[i].y) <= bSize / 2 + obstacles[i].size / 2) {
-   if (obstacles[i].type === "obs") {
-=======
 }
 if (timer<=0){
   if(score>=20){
@@ -177,56 +122,43 @@ else if(level==3){
  for (i = 0; i < iMax; i++) {
     if (dist(posX, posY, obstacles[i].x,obstacles[i].y) <= bSize / 2 + obstacles[i].size / 2){
     if(obstacles[i].type==="obs"){
->>>>>>> origin/master
     vitesseX = -vitesseX * bounce;
-    vitesseY = -vitesseY * bounce;
-   } else if (obstacles[i].type === "bonus") {
-    if (obstacles[i].col === false) {
-     obstacles[i].col = true
-     score += 5;
-    }
-   } else if (obstacles[i].type === "malus") {
-    if (obstacles[i].col === false) {
-     obstacles[i].col = true
-     score -= 5;
-    }
+     vitesseY = -vitesseY * bounce;
+    }else if (obstacles[i].type==="bonus") {
+      if (obstacles[i].col === false) {
+       obstacles[i].col = true
+       score += 5;
+      }
+     }else if (obstacles[i].type==="malus") {
+      if (obstacles[i].col === false) {
+       obstacles[i].col = true
+       score -= 5;
+      }
+     }
    }
-  } else {
+    else {
    obstacles[i].col = false;
-  }
+    }
  }
 }
-<<<<<<< HEAD
-
-function drawBall() {
- fill(0, 255, 255);
- ellipse(posX, posY, bSize, bSize);
-}
-
-function Obs(obstacle, size, x, y) {
-=======
 /////////////////////////////////////////////////
 function Obs(obstacle) {
->>>>>>> origin/master
  this.type = obstacle;
- this.x = x;
- this.y = y;
- this.size = size;
- this.col = false
- if (this.type === "obs") {
-  this.colour = color(0, 0, 255);
- } else if (this.type === "bonus") {
-  this.colour = color(0, 255, 0);
- } else {
-  this.colour = color(255, 0, 0);
- }
+ this.x = random(10, windowWidth);
+ this.y = random(10, windowHeight);
+ this.size = random(30, 60);
+ this.col=false
+if(this.type==="obs"){
+   this.colour=color(0,0,255);
+    }else if (this.type==="bonus") {
+      this.colour=color(0,255,0);
+     }else{
+      this.colour=color(255,0,0);
+      }
  this.drawObs = function() {
   fill(this.colour)
   ellipse(this.x, this.y, this.size, this.size);
  }
-<<<<<<< HEAD
-}
-=======
 }
 ///////////////////////////////////////////
 function gameOver(){
@@ -236,4 +168,3 @@ function gameOver(){
    rect(windowWidth/2,windowHeight/2,50,50);
    text("GAME OVER",windowWidth/2,windowHeight/2);
 }
->>>>>>> origin/master
